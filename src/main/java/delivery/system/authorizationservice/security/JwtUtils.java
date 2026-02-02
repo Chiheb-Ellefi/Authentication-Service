df45d81055b275e35ecf4a3d23cdb8a5cf31d704
chihebellefi <chihebellefi888@gmail.com>
2026-02-02 08:07:08 +0100

    updated jwtUtils

--- src/main/java/delivery/system/authorizationservice/security/JwtUtils.java
+++ src/main/java/delivery/system/authorizationservice/security/JwtUtils.java
@@ -1,6 +1,22 @@
 package delivery.system.authorizationservice.security;
 
+import delivery.system.authorizationservice.models.CustomUserDetails;
+import io.jsonwebtoken.Jwts;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
+@Component
 public class JwtUtils {
+
+
+    public String generateToken(CustomUserDetails user) {
+        return Jwts.builder()
+                .subject(user.getUsername())
+                .claim("username", user.getUsername())
+                .claim("authorities", user.getAuthorities())
+                .claim("roles", user.getAuthorities())
+
+                .compact();
+    }
 
 }
